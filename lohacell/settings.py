50f160e173d38ad7333a93a54e619a1eb950dc9f
furixon <steve@furixon.com>
2023-03-26 19:26:23 +0900

    20230326 update 2

--- lohacell/settings.py
+++ lohacell/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qc!$i3nlg9yy&y8%)vji2a_+1ei-ekp&rf8sbqx^w99m^&xkpd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
